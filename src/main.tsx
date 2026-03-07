a27b97cfcca1981970fd475d8d2ca73f257bb3f7
Brian Davis <kbdavis07@yahoo.com>
2026-03-07 12:55:23 -0500

    feat: integrate Swetrix analytics for tracking user interactions

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,10 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
+  import * as Swetrix from 'swetrix'
+
+  Swetrix.init('qh32mFcUeEUc')
+  Swetrix.trackViews()
+
   createRoot(document.getElementById("root")!).render(<App />);
   
